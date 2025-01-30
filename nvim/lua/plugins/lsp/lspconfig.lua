34ef9188529179501923c898b2d5a6254b304c97
Crypto-Spartan <accounts@cryptospartan.net>
2025-01-30 01:07:26 -0500

    add parenthesis around function parameters

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -110,7 +110,7 @@ return {
                 -- This may be unwanted, since they displace some of your code
                 if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
                     map('<leader>th', function()
-                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
                     end, 'Toggle Hints')
                     vim.lsp.inlay_hint.enable(true)
                     local toggle_opts = {
