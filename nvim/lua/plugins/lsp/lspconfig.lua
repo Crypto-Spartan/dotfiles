d5042355f04f57ee510506e06dae483649a628c4
Crypto-Spartan <accounts@cryptospartan.net>
2025-04-06 18:03:14 -0400

    remove unneeded comments that came from kickstart.nvim

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,36 +1,18 @@
 return {
-    -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     event = { 'LazyFileOpen', 'BufNewFile' },
     dependencies = {
-        -- Automatically install LSPs and related tools to stdpath for Neovim
         'williamboman/mason.nvim', -- NOTE: Must be loaded before dependants
         'williamboman/mason-lspconfig.nvim',
         'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-        -- navigation that hooks into the LSP
         'SmiteshP/nvim-navbuddy',
-
-        -- Useful status updates for LSP.
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
         'j-hui/fidget.nvim',
-
-        -- Allows extra capabilities provided by nvim-cmp
         'hrsh7th/cmp-nvim-lsp',
     },
     init = function()
-        --  This function gets run when an LSP attaches to a particular buffer.
-        --    That is to say, every time a new file is opened that is associated with
-        --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-        --    function will be executed to configure the current buffer
         vim.api.nvim_create_autocmd('LspAttach', {
             group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
             callback = function(event)
-                -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-                -- to define small helper and utility functions so you don't have to repeat yourself.
-
-                -- In this case, we create a function that lets us more easily define mappings specific
-                -- for LSP related items. It sets the mode, buffer and description for us each time.
                 local map = function(keys, func, desc, mode)
                     mode = mode or 'n'
                     vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
@@ -45,7 +27,6 @@ return {
                 --  For example, in C this would take you to the header.
                 map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
-                -- Find references for the word under your cursor.
                 map('gr', require('telescope.builtin').lsp_references, 'Goto References')
 
                 -- Jump to the implementation of the word under your cursor.
@@ -65,14 +46,8 @@ return {
                 --  Similar to document symbols, except searches over your entire project.
                 map('<leader>fS', require('telescope.builtin').lsp_document_symbols, 'Find Symbols (Workspace)')
 
-                -- Rename the variable under your cursor.
-                --  Most Language Servers support renaming across files, etc.
                 map('<leader>lr', vim.lsp.buf.rename, 'Rename')
-
-                -- Execute a code action, usually your cursor needs to be on top of an error
-                -- or a suggestion from your LSP for this to activate.
                 map('<leader>la', vim.lsp.buf.code_action, 'Action', {'n','x'})
-
                 map('<leader>lR', vim.cmd.LspRestart, 'Restart')
 
                 -- The following two autocommands are used to highlight references of the
@@ -104,10 +79,6 @@ return {
                 --     })
                 -- end
 
-                -- The following code creates a keymap to toggle inlay hints in your
-                -- code, if the language server you are using supports them
-                --
-                -- This may be unwanted, since they displace some of your code
                 if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
                     map('<leader>th', function()
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
@@ -132,40 +103,11 @@ return {
         })
     end,
     config = function()
-        -- LSP servers and clients are able to communicate to each other what features they support.
-        --  By default, Neovim doesn't support everything that is in the LSP specification.
-        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-        -- Enable the following language servers
-        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-        --
-        --  Add any additional override configuration in the following tables. Available keys are:
-        --  - cmd (table): Override the default command used to start the server
-        --  - filetypes (table): Override the default list of associated filetypes for the server
-        --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-        --  - settings (table): Override the default settings passed when initializing the server.
-        --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
-            -- clangd = {},
-            -- gopls = {},
-            -- pyright = {},
-            -- rust_analyzer = {},
-            -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-            --
-            -- Some languages (like typescript) have entire language plugins that can be useful:
-            --    https://github.com/pmizio/typescript-tools.nvim
-            --
-            -- But for many setups, the LSP (`ts_ls`) will work just fine
-            -- ts_ls = {},
-            --
-
             lua_ls = {
-                -- cmd = {...},
-                -- filetypes = { ...},
-                -- capabilities = {},
                 settings = {
                     Lua = {
                         runtime = { version = 'LuaJIT' },
@@ -200,16 +142,9 @@ return {
             -- },
         }
 
-        -- Ensure the servers and tools above are installed
-        --  To check the current status of installed tools and/or manually install
-        --  other tools, you can run
-        --    :Mason
-        --
-        --  You can press `g?` for help in this menu.
         require('mason').setup()
 
-        -- You can add other tools here that you want Mason to install
-        -- for you, so that they are available from within Neovim.
+        -- You can add other tools here that you want Mason to install for you, so that they are available from within Neovim.
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
             -- 'gopls',
