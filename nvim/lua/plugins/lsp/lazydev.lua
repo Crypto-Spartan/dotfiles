968b7d741879ba59500358e20fdecf9e34a22e97
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-07-11 21:21:40 -0400

    Update lazydev.lua

--- nvim/lua/plugins/lsp/lazydev.lua
+++ nvim/lua/plugins/lsp/lazydev.lua
@@ -1,6 +1,8 @@
 return {
     'folke/lazydev.nvim',
     ft = 'lua',
+    cmd = 'LazyDev',
+    -- dependencies = { 'Bilal2453/luvit-meta' },
     opts = {
         library = {
             -- load luvit types when the `vim.uv` word is found
