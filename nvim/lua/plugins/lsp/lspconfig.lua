e2287753844942db1ecde5f39632718417b0dc86
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-07-11 21:29:03 -0400

    Update lspconfig.lua

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -57,7 +57,7 @@ return {
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
                 -- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-                --     local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+                --     local highlight_augroup = vim.api.nvim_create_augroup('lspconfig-lsp-highlight', { clear = false })
                 --     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 --         buffer = event.buf,
                 --         group = highlight_augroup,
@@ -71,19 +71,17 @@ return {
                 --     })
                 --
                 --     vim.api.nvim_create_autocmd('LspDetach', {
-                --         group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                --         group = vim.api.nvim_create_augroup('lspconfig-lsp-detach', { clear = true }),
                 --         callback = function(event2)
                 --             vim.lsp.buf.clear_references()
-                --             vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                --             vim.api.nvim_clear_autocmds { group = 'lspconfig-lsp-highlight', buffer = event2.buf }
                 --         end,
                 --     })
                 -- end
 
                 if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-                    map('<leader>th', function()
-                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-                    end, 'Toggle Hints')
                     vim.lsp.inlay_hint.enable(true)
+                    -- snacks.toggle setup
                     local toggle_opts = {
                         name = 'Hints (LSP)',
                         get = function()
