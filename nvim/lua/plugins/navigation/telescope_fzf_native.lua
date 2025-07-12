8ffdc3e1d0a7ac919219240681772b7a3d0b784a
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-07-11 21:48:12 -0400

    Update telescope_fzf_native.lua

--- nvim/lua/plugins/navigation/telescope_fzf_native.lua
+++ nvim/lua/plugins/navigation/telescope_fzf_native.lua
@@ -4,12 +4,11 @@ return {
     lazy = true,
 
     -- `build` is used to run some command when the plugin is installed/updated
-    -- this is only run then, not ever time neovim starts up
+    -- this is only run then, not every time neovim starts up
     build = 'make',
     -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
 
-    -- `cond` is a condition used to determine whether this plugin should be installed & loaded
     cond = function()
-        return vim.fn.executable 'make' == 1
+        return vim.fn.executable('make') == 1
     end,
 }
