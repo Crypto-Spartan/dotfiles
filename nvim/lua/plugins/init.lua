184e3cd62e975a89f6c6729b087650177873ecce
Crypto-Spartan <accounts@cryptospartan.net>
2025-09-26 01:30:10 -0400

    fix init.lua: comment out plugins.custom for now

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
     { import = 'plugins.autocomplete' },
-    { import = 'plugins.custom' },
+    -- { import = 'plugins.custom' },
     { import = 'plugins.lang_specific' },
     { import = 'plugins.lsp' },
     { import = 'plugins.navigation' },
