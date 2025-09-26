a3178aa2dee1e3122191faa03fad71d5c4340a07
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-09-25 23:10:54 -0400

    fix lspconfig gt keybind

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -43,7 +43,7 @@ return {
                 -- Jump to the type of the word under your cursor.
                 --  Useful when you're not sure what type a variable is and you want to see
                 --  the definition of its *type*, not where it was *defined*.
-                map('gt', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
+                map('<leader>gt', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
 
                 -- Fuzzy find all the symbols in your current document.
                 --  Symbols are things like variables, functions, types, etc.
