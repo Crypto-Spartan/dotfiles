c4aa142876cf88fccd760205d35d7151776b07b2
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-09-25 20:33:45 -0400

    Update init.lua - add `plugins.custom`

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
     { import = 'plugins.autocomplete' },
+    { import = 'plugins.custom' },
     { import = 'plugins.lang_specific' },
     { import = 'plugins.lsp' },
     { import = 'plugins.navigation' },
