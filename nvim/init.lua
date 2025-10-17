935d801d18464f070fda858f565f1a1caf7cf17e
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-10-17 16:55:14 -0400

    Change profiler event to 'User' for LazyFileOpen

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ if vim.env.PROF then
     vim.opt.rtp:append(snacks)
     require('snacks.profiler').startup({
         startup = {
-            -- event = 'VeryLazy', -- stop profiler on this event (default is `VimEnter`)
+            -- event = 'VimEnter', -- stop profiler on this event (default is `VimEnter`)
             event = 'User',
             pattern = 'LazyFileOpen',
             after = true
