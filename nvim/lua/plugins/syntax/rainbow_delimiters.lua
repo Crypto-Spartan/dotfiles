1f22cfde3ff3d189ad033ebcb6b5f2b2822de207
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-10-17 16:38:51 -0400

    Update rainbow_delimiters.lua

--- nvim/lua/plugins/syntax/rainbow_delimiters.lua
+++ nvim/lua/plugins/syntax/rainbow_delimiters.lua
@@ -2,7 +2,6 @@ return {
     'HiPhish/rainbow-delimiters.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     event = { 'LazyFileOpen', 'LazyOilPreview', 'LazyTelescopePreview', 'BufNewFile' },
-    main = 'rainbow-delimiters.setup',
     init = function()
         vim.g.rainbow_delimiters = {
             highlight = {
@@ -16,9 +15,23 @@ return {
             }
         }
     end,
-    opts = function()
-        return { highlight = vim.g.rainbow_delimiters.highlight }
-    end,
+    config = function()
+        -- snacks toggle setup
+        vim.schedule(function()
+            local rd_pkg = require('rainbow-delimiters')
+            rd_pkg.enable(0)
+            local toggle_opts = {
+                name = 'Rainbow Delimiters',
+                get = function()
+                    return rd_pkg.is_enabled(0)
+                end,
+                set = function(_)
+                    rd_pkg.toggle(0)
+                end
+            }
+            package.loaded.snacks.toggle.new(toggle_opts):map('<leader>tr')
+        end)
+    end
 }
 
 -- default order:
